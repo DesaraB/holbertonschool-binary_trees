7a13e23813f8f72ba29ac295ea3f20109b8ec28f
“DesaraB” <desarabaroti99@gmail.com>
2024-12-11 13:17:43 +0000

    BT Leaves

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,20 +1,25 @@
-#include "binary_trees.h"
+#include"binary_trees.h"
 
 /**
- * binary_tree_leaves - Counts the leaves in a binary tree.
- * @tree: A pointer to the root node of the tree to count the leaves of.
- *
- * Return: The number of leaves in the tree.
+ * binary_tree_leaves - counts the number of the leaves in a binary tree
+ * @tree: -points to the root of the tree
+ * Return: (leaves)
  */
+
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t leaves = 0;
+	size_t left_leaves = 0;
+	size_t right_leaves = 0;
+	size_t leaves;
 
-	if (tree)
-	{
-		leaves += (!tree->left && !tree->right) ? 1 : 0;
-		leaves += binary_tree_leaves(tree->left);
-		leaves += binary_tree_leaves(tree->right);
-	}
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	if (tree->left != NULL)
+		left_leaves = binary_tree_leaves(tree->left);
+	if (tree->right != NULL)
+		right_leaves = binary_tree_leaves(tree->right);
+	leaves = left_leaves + right_leaves;
 	return (leaves);
 }
